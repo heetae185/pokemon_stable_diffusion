5bce2bbb29956764cd1b820068f8524bece0b6c9
heetae185 <heetae185@naver.com>
2023-06-03 21:21:14 +0900

    gpu settings

--- finetune.py
+++ finetune.py
@@ -13,6 +13,8 @@ import os
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
+import tensorflow as tf
+
 physical_devices = tf.config.list_physical_devices('GPU')
 for device in physical_devices:
     tf.config.experimental.set_memory_growth(device, True)
@@ -20,7 +22,7 @@ for device in physical_devices:
 
 import argparse
 
-import tensorflow as tf
+
 from keras_cv.models.stable_diffusion.diffusion_model import DiffusionModel
 from keras_cv.models.stable_diffusion.image_encoder import ImageEncoder
 from keras_cv.models.stable_diffusion.noise_scheduler import NoiseScheduler
